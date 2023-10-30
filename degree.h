6929eda65d358a4b0e749fe77dcedb2a0583294d
Kenneth Lee <klee684@wgu.edu>
2023-10-30 17:08:24 -0600

    Clean up

--- degree.h
+++ degree.h
@@ -1,6 +1,3 @@
 #pragma once
 
 	enum class DegreeProgram {SECURITY, NETWORK, SOFTWARE};
-
-	//Array of strings
-	//static const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
